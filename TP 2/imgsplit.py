28597d0d141be8dbaef4e98a224f730d82b77bf2
Anthony Lachance <alach987@yahoo.ca>
2019-02-24 14:08:56 -0500

    split into 8x8 regions done

--- TP 2/imgsplit.py
+++ TP 2/imgsplit.py
@@ -1,10 +1,16 @@
 import numpy as np
 
+
 def imgsplit(img):
-    split_img = np.empty([0])
     sections_x = np.size(img, axis=0) / 8
     sections_y = np.size(img, axis=1) / 8
-    x_split = np.split(img, sections_x, axis=0)
-    for im in x_split:
-        np.split(im, sections_y, axis=1)
-        split_img[]
+    split_img = np.reshape(img,(sections_x,sections_y,8,8))
+    #x_split = np.split(img, sections_x, axis=0)
+    #y_split = np.split(x_split, sections_y, axis=1)
+    return split_img
+
+def imgunsplit(img):
+    size_x = np.size(img, axis=0) * np.size(img, axis=2)
+    size_y = np.size(img, axis=1) * np.size(img, axis=3)
+    unsplit_img = np.reshape(img, (size_x, size_y))
+    return unsplit_img;
--- TP 2/main.py
+++ TP 2/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 import rgb2ycbcr
+import imgsplit
 
 img = np.asarray(Image.open("Lena_512.png"))
 img_no_alpha = img[:,:,:3]
@@ -13,25 +14,34 @@ plt.figure()
 plt.imshow(img_no_alpha, cmap=plt.get_cmap('gray'))
 plt.draw()
 
+#conversion to YCbCr
 immod = rgb2ycbcr.rgb2ycbcr(img_no_alpha)
 plt.figure()
 plt.imshow(immod, cmap=plt.get_cmap('gray'))
 plt.draw()
 
-
+#Split into 8x8 regions
 Y = immod[:,:,0]
 Cb = immod[:,:,1]
 Cr = immod[:,:,2]
 
+Ysplit = imgsplit.imgsplit(Y)
+Cbsplit = imgsplit.imgsplit(Cb)
+Crsplit = imgsplit.imgsplit(Cr)
 
 
 
+#convert back
 
-# convert back
+#recombine the 8x8 regions
+Yunsplit = imgsplit.imgunsplit(Ysplit)
+Cbunsplit = imgsplit.imgunsplit(Cbsplit)
+Crunsplit = imgsplit.imgunsplit(Crsplit)
 
+im_recombine = np.dstack((Yunsplit, Cbunsplit, Crunsplit))
 
-
-immod = rgb2ycbcr.ycbcr2rgb(immod)
+#convert back to RGB
+immod = rgb2ycbcr.ycbcr2rgb(im_recombine)
 plt.figure()
 plt.imshow(immod, cmap=plt.get_cmap('gray'))
 plt.draw()
